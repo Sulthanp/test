7688dbdecf98a373d2bb3e4b2b09f3a3df7cd795
Sulthan Palagiri <sulthanpalagiri83@gmail.com>
2025-07-06 15:54:32 +0000

    k

--- models/staging/appends.sql
+++ models/staging/appends.sql
@@ -3,4 +3,4 @@
     incremental_strategy="append",
 ) }}
 
-select * from {{source('datafeed_shared_schema','orders')}} where id in (1,2,3)
+select * from {{source('datafeed_shared_schema','orders')}}
